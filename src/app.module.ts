b700d5c32f9013e0af270642c3e1756a689698f9
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-16 17:22:55 +0900

    feat: user resource 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { DatabaseModule } from './database/database.module';
       }),
     }),
     DatabaseModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
