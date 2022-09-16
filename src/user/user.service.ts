b32d8b6fc038a53e28cffa1c6d812bf2df2dfd3e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-16 17:44:10 +0900

    feat: user 생성 기능

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserResponseDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
 
-  findAll() {
-    return `This action returns all user`;
+  /**
+   * User를 생성하고 생성된 user의 id를 반환합니다.
+   * @returns { userId }
+   */
+  async create(): Promise<CreateUserResponseDto> {
+    const user = await this.userRepository.create();
+    await this.userRepository.save(user);
+    return {
+      userId: user.id,
+    };
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
