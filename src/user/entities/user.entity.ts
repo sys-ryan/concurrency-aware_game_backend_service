16e46cfd0eb76099acf19f30e916f19c4033d678
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-16 18:23:45 +0900

    feat: entities 생성
    - user
    - boss-raid-history

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,11 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BossRaidHistory } from 'src/boss-raid-history/entities/boss-raid-history.entity';
+import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @OneToMany(() => BossRaidHistory, (bossRaidHistory) => bossRaidHistory.user)
+  bossRaidHistories: BossRaidHistory[];
 }
