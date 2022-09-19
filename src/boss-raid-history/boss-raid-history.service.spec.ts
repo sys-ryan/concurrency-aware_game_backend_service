dd664155aba08190210a784e98a50a6c324fd01e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-20 02:42:41 +0900

    test: 보스레이드 상태 조회 Unit 테스트

--- src/boss-raid-history/boss-raid-history.service.spec.ts
+++ src/boss-raid-history/boss-raid-history.service.spec.ts
@@ -8,11 +8,37 @@ import { UserService } from '../user/user.service';
 import { HttpService } from '@nestjs/axios';
 import { CACHE_MANAGER } from '@nestjs/common';
 
-const mockCacheManager = () => ({});
+const bossRaidHistories: BossRaidHistory[] = [];
+
+const bossRaidAvailability: BossRaidAvailability[] = [];
+
+const mockCacheManager = () => ({
+  get: jest.fn().mockImplementation(() => ({
+    bossRaidLimitSeconds: 180,
+    levels: [
+      {
+        level: 0,
+        score: 20,
+      },
+      {
+        level: 1,
+        score: 47,
+      },
+      {
+        level: 2,
+        score: 85,
+      },
+    ],
+  })),
+});
 
 const mockBossRaidHistoryRepository = () => ({});
 
-const mockBossRaidAvailabilityRepository = () => ({});
+const mockBossRaidAvailabilityRepository = () => ({
+  find: jest.fn().mockImplementation(() => {
+    return bossRaidAvailability;
+  }),
+});
 
 const mockDataSource = () => ({});
 
@@ -57,7 +83,71 @@ describe('BossRaidHistoryService', () => {
     service = module.get<BossRaidHistoryService>(BossRaidHistoryService);
   });
 
+  afterEach(async () => {
+    bossRaidHistories.splice(0, bossRaidHistories.length);
+    bossRaidAvailability.splice(0, bossRaidAvailability.length);
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('보스레이드 상태 조회', () => {
+    it('보스레이드를 시작한 기록이 없다면 {canEnter:true} 반환', async () => {
+      const status = await service.getBossRaidStatus();
+
+      expect(status.canEnter).toBe(true);
+      expect(status.enteredUserId).not.toBeDefined();
+    });
+
+    it('보스레이드를 플레이중인 유저가 있다면 {canEnter: false} 반환', async () => {
+      bossRaidAvailability.push({
+        canEnter: false,
+        enteredAt: new Date(),
+        id: 1,
+        userId: 1,
+      });
+
+      const status = await service.getBossRaidStatus();
+
+      expect(status).toBeDefined();
+      expect(status.canEnter).toBe(false);
+      expect(status.enteredUserId).toBe(1);
+    });
+
+    it('마지막 시작한 시간으로부터 레이드 제한 시간 만큼 경과되었으면 {canEnter: true} 반환', async () => {
+      const date = new Date();
+      date.setMinutes(date.getMinutes() - 4);
+
+      bossRaidAvailability.push({
+        canEnter: false,
+        enteredAt: date,
+        id: 1,
+        userId: 1,
+      });
+
+      const status = await service.getBossRaidStatus();
+
+      expect(status).toBeDefined();
+      expect(status.canEnter).toBe(true);
+    });
+  });
+
+  // describe('보스레이드 입장', () => {
+  //   it('존재하지 않는 userId로 요청시 예외 처리', async () => {});
+
+  //   it('존재하지 않는 level로 요청시 예외 처리', async () => {});
+
+  //   it('{canEnter: false}일 때 입장 요청시 {isEntered: false} 반환', async () => {});
+  // });
+
+  // describe('보스레이드 종료', () => {
+  //   it('', async () => {});
+
+  //   it('', async () => {});
+
+  //   it('', async () => {});
+
+  //   it('', async () => {});
+  // });
 });
