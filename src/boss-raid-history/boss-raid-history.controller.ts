b8c8e1b1383288022cb8230db183d486d7d79d54
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-19 17:56:50 +0900

    docs: Swagger API Documentation

--- src/boss-raid-history/boss-raid-history.controller.ts
+++ src/boss-raid-history/boss-raid-history.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, HttpCode } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  PartialType,
+} from '@nestjs/swagger';
 import {
   BossRaidHistoryService,
   RankingData,
@@ -9,19 +16,41 @@ import {
   EnterBossRaidResponseDto,
 } from './dto/enter-boss-raid.dto';
 import { GetBossRaidStatusResponseDto } from './dto/get-boss-raid-status.dto';
-import { GetRankingInfoListDto } from './dto/get-ranking-info.dto';
+import {
+  GetRankingInfoListDto,
+  GetRankingInfoListResponseDto,
+} from './dto/get-ranking-info.dto';
 
+@ApiTags('BossRaid API')
 @Controller('bossRaid')
 export class BossRaidHistoryController {
   constructor(
     private readonly bossRaidHistoryService: BossRaidHistoryService,
   ) {}
 
+  @ApiOperation({
+    summary: '보스레이드 상태 조회 API',
+    description:
+      '보스레이드 현재 상태를 응답합니다. (입장 가능 여부, 현재 진행중인 유저가 있다면, 해당 유저의 id)',
+  })
+  @ApiOkResponse({
+    description:
+      '보스레이드 현재 상태를 응답합니다. (입장 가능 여부, 현재 진행중인 유저가 있다면, 해당 유저의 id)',
+    type: GetBossRaidStatusResponseDto,
+  })
   @Get()
   getBossRaidStatus(): Promise<Partial<GetBossRaidStatusResponseDto>> {
     return this.bossRaidHistoryService.getBossRaidStatus();
   }
 
+  @ApiOperation({
+    summary: '보스레이드 랭킹 조회 API',
+    description: '보스레이드 totalScore 내림차순으로 랭킹을 조회합니다.',
+  })
+  @ApiOkResponse({
+    description: '보스레이드 totalScore 내림차순으로 랭킹을 조회합니다.',
+    type: GetRankingInfoListResponseDto,
+  })
   @Get('topRankerList')
   getRankingInfoList(
     @Body() getRankingInfoListDto: GetRankingInfoListDto,
@@ -31,6 +60,15 @@ export class BossRaidHistoryController {
     );
   }
 
+  @ApiOperation({
+    summary: '보스레이드 시작 API',
+    description: '레이드가 시작 가능하다면 보스레이드를 시작합니다.',
+  })
+  @ApiCreatedResponse({
+    description: '레이드가 시작 가능하다면 보스레이드를 시작합니다.',
+    type: EnterBossRaidResponseDto,
+  })
+  @HttpCode(201)
   @Post('enter')
   enterBossRaid(
     @Body() enterBossRaidDto: EnterBossRaidDto,
@@ -38,6 +76,13 @@ export class BossRaidHistoryController {
     return this.bossRaidHistoryService.enterBossRaid(enterBossRaidDto);
   }
 
+  @ApiOperation({
+    summary: '보스레이드 종료 API',
+    description: '보스레이드를 종료합니다.',
+  })
+  @ApiOkResponse({
+    description: '보스레이드를 종료합니다.',
+  })
   @Patch('end')
   endBossRaid(@Body() endBossRaidDto: EndBossRaidDto): Promise<void> {
     return this.bossRaidHistoryService.endBossRaid(endBossRaidDto);
