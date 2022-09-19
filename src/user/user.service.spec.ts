a22b078feec74fc640942288bf8b02f0ade7815c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-20 00:56:37 +0900

    test: User service unit 테스트
    - 유저 생성 기능
    - 유저 생성시 고유한 id

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -7,15 +7,13 @@ import { UserService } from './user.service';
 const users: User[] = [];
 
 const mockUserRepository = () => ({
-  create: jest.fn(),
-  save: jest.fn(),
+  create: jest.fn().mockImplementation(() => ({
+    id: Math.random(),
+  })),
+  save: jest.fn().mockImplementation((user) => {
+    users.push(user);
+  }),
   findOne: jest.fn(),
-});
-
-const mockUserService = () => ({
-  create: jest.fn(),
-  findOne: jest.fn(),
-  findById: jest.fn(),
 });
 
 const mockDataSource = () => ({});
@@ -32,10 +30,6 @@ describe('UserService', () => {
           useValue: mockUserRepository(),
         },
         {
-          provide: UserService,
-          useValue: mockUserService(),
-        },
-        {
           provide: DataSource,
           useValue: mockDataSource(),
         },
@@ -47,5 +41,21 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('유저 생성 기능', async () => {
+    const user = await service.create();
+    console.log(user);
+    expect(user).toBeDefined();
+  });
+
+  it('유저 생성시 고유한 id 생성 검증', async () => {
+    const user1 = await service.create();
+    const user2 = await service.create();
+    const user3 = await service.create();
+
+    expect(user1.userId).not.toEqual(user2.userId);
+    expect(user1.userId).not.toEqual(user3.userId);
+    expect(user2.userId).not.toEqual(user3.userId);
   });
 });
