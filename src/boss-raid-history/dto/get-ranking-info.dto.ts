2104b82c1d8a93565b046b74940facb6ebd114af
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-19 17:56:50 +0900

    docs: Swagger API Documentation

--- src/boss-raid-history/dto/get-ranking-info.dto.ts
+++ src/boss-raid-history/dto/get-ranking-info.dto.ts
@@ -7,3 +7,20 @@ export class GetRankingInfoListDto {
   @IsNumber()
   userId: number;
 }
+
+/**
+ * 보스레이드 랭킹 Info
+ */
+export class RankingInfo {
+  rainking: number;
+  userId: number;
+  totalScore: number;
+}
+
+/**
+ * 보스레이드 랭킹 조회 response
+ */
+export class GetRankingInfoListResponseDto {
+  topRankerInfoList: RankingInfo[];
+  myRankingInfo: RankingInfo;
+}
