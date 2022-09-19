c167405fa6ca09f908a66e4137751f891bee8a84
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-20 01:06:23 +0900

    test: BossRaid Unit 테스트 환경 세팅

--- src/boss-raid-history/boss-raid-history.service.spec.ts
+++ src/boss-raid-history/boss-raid-history.service.spec.ts
@@ -1,12 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { BossRaidHistoryService } from './boss-raid-history.service';
+import { BossRaidAvailability } from './entities/boss-raid-availability.entity';
+import { BossRaidHistory } from './entities/boss-raid-history.entity';
+import { DataSource } from 'typeorm';
+import { UserService } from '../user/user.service';
+import { HttpService } from '@nestjs/axios';
+import { CACHE_MANAGER } from '@nestjs/common';
+
+const mockCacheManager = () => ({});
+
+const mockBossRaidHistoryRepository = () => ({});
+
+const mockBossRaidAvailabilityRepository = () => ({});
+
+const mockDataSource = () => ({});
+
+const mockUserService = () => ({});
+
+const mockHttpService = () => ({});
 
 describe('BossRaidHistoryService', () => {
   let service: BossRaidHistoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BossRaidHistoryService],
+      providers: [
+        BossRaidHistoryService,
+        {
+          provide: getRepositoryToken(BossRaidHistory),
+          useValue: mockBossRaidHistoryRepository(),
+        },
+        {
+          provide: getRepositoryToken(BossRaidAvailability),
+          useValue: mockBossRaidAvailabilityRepository(),
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager(),
+        },
+        {
+          provide: DataSource,
+          useValue: mockDataSource(),
+        },
+        {
+          provide: UserService,
+          useValue: mockUserService(),
+        },
+        {
+          provide: HttpService,
+          useValue: mockHttpService(),
+        },
+      ],
     }).compile();
 
     service = module.get<BossRaidHistoryService>(BossRaidHistoryService);
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
+
+const users: User[] = [];
+
+const mockUserRepository = () => ({
+  create: jest.fn().mockImplementation(() => ({
+    id: Math.random(),
+  })),
+  save: jest.fn().mockImplementation((user) => {
+    users.push(user);
+  }),
+  findOne: jest.fn(),
+});
+
+const mockDataSource = () => ({});
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository(),
+        },
+        {
+          provide: DataSource,
+          useValue: mockDataSource(),
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +41,21 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('유저 생성 기능', async () => {
+    const user = await service.create();
+    console.log(user);
+    expect(user).toBeDefined();
+  });
+
+  it('유저 생성시 고유한 id 생성 검증', async () => {
+    const user1 = await service.create();
+    const user2 = await service.create();
+    const user3 = await service.create();
+
+    expect(user1.userId).not.toEqual(user2.userId);
+    expect(user1.userId).not.toEqual(user3.userId);
+    expect(user2.userId).not.toEqual(user3.userId);
   });
 });
