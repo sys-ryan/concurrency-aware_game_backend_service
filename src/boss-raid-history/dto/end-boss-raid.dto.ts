26b2a14216f404ed2aa429e5a58f86586ceb6dcc
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-19 18:09:23 +0900

    docs: Swagger documentation for types

--- src/boss-raid-history/dto/end-boss-raid.dto.ts
+++ src/boss-raid-history/dto/end-boss-raid.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, Min } from 'class-validator';
 
 /**
  * 보스레이드 종료 request body
  */
 export class EndBossRaidDto {
+  @ApiProperty({ description: '보스레이드를 플레이한 유저 id', minimum: 1 })
   @IsNumber()
   @Min(1)
   userId: number;
 
+  @ApiProperty({ description: '보스레이드 recordId', minimum: 1 })
   @IsNumber()
   @Min(1)
   raidRecordId: number;
--- src/boss-raid-history/dto/enter-boss-raid.dto.ts
+++ src/boss-raid-history/dto/enter-boss-raid.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsPositive, Min } from 'class-validator';
 
 /**
  * 보스레이드 시작 request body
  */
 export class EnterBossRaidDto {
+  @ApiProperty({ description: '보스레이드 시작 유저 id', minimum: 1 })
   @IsNumber()
   @IsPositive()
   userId: number;
 
+  @ApiProperty({ description: '보스레이드 레벨' })
   @IsNumber()
   @Min(0)
   level: number;
@@ -17,7 +20,10 @@ export class EnterBossRaidDto {
  * 보스레이드 시작 response
  */
 export class EnterBossRaidResponseDto {
+  @ApiProperty({ description: '보스레이드 입장 여부' })
   isEntered: boolean;
 
+  @ApiProperty({ description: '보스레이드 recordId', minimum: 1 })
+  @ApiPropertyOptional()
   raidRecordId: number;
 }
