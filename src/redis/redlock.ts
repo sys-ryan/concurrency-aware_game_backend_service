6e9a8d6938e8a92ea7e7f76f5d9a675d0b37c11c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-20 22:55:25 +0900

    fix: fix redlock conf

--- src/redis/redlock.ts
+++ src/redis/redlock.ts
@@ -3,9 +3,14 @@ import Redlock from 'redlock';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: `../../*.env` });
-export const redis = new Client.Cluster([
-  { host: process.env.REDIS_HOST, port: +process.env.REDIS_PORT },
-]);
+export const redis = new Client.Cluster(
+  [{ host: process.env.REDIS_HOST, port: +process.env.REDIS_PORT }],
+  {
+    redisOptions: {
+      tls: {},
+    },
+  },
+);
 
 export const redlock = new Redlock(
   [redis],
